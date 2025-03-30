5f0f1f261ab773d058fbcdfb45f308397c807feb
Kuhle-Mavundla <hlosokuhlemavundla@gmail.com>
2025-03-30 20:39:51 +0200

    added controllers and modified scaffolding

---
+++ EventEaseAssignment1/Data/EventEaseAssignment1Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EventEaseAssignment1.Models;
+
+namespace EventEaseAssignment1.Data
+{
+    public class EventEaseAssignment1Context : DbContext
+    {
+        public EventEaseAssignment1Context (DbContextOptions<EventEaseAssignment1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EventEaseAssignment1.Models.Venue> Venue { get; set; } = default!;
+        public DbSet<EventEaseAssignment1.Models.Event> Event { get; set; } = default!;
+        public DbSet<EventEaseAssignment1.Models.Booking> Booking { get; set; } = default!;
+    }
+}
--- EventEaseAssignment1/Program.cs
+++ EventEaseAssignment1/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using EventEaseAssignment1.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<EventEaseAssignment1Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("EventEaseAssignment1Context") ?? throw new InvalidOperationException("Connection string 'EventEaseAssignment1Context' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
